4cf6bb2d9542a431c79a942f61b99085b0f353d1
sdanilov <sdanilov@kirkazan.ru>
2015-07-08 15:23:06 +0300

    [nf]

--- server/x.js
+++ server/x.js
@@ -6,19 +6,41 @@ randWDclassic = function(n){
     return s;
 };
 
-genRandomMeta = function(){
-    var meta = {};
-    var count = Math.round((Math.random() * 10 + 10)); //количество параметров
+
+function randomNumber (from, to) {
+    return Math.round((Math.random() * (to - from) + from));
+}
+var genRandomMeta = function(from, to){
+    var meta = [];
+    var count = randomNumber(from, to); //количество параметров
 
     for (var i = 0; i < count; i++){
-        meta[randWDclassic(10)] = randWDclassic(10);
+        var m = {
+            _type: typeList[randomNumber(0, typeList.length)]
+        };
+        var fieldCount = randomNumber(10, 20);
+        for (var j = 0; j < fieldCount; j++) {
+            m[randWDclassic(10)] = randWDclassic(20);
+        }
+        meta[i] = m;
     }
     return meta;
 };
 
-genRandomTags = function(){
+
+var genRandomTags = function(from, to){
     var tags = [];
-    var count = Math.round((Math.random() * 10 + 10)); //количество тегов
+    var count = randomNumber(from, to); //количество тегов
+
+    for (var i = 0; i < count; i++){
+        tags[i] = tagList[randomNumber(0, tagList.length)];
+    }
+    return tags;
+};
+
+var genRandomTagsList = function(from, to){
+    var tags = [];
+    var count = randomNumber(from, to); //количество тегов
 
     for (var i = 0; i < count; i++){
         tags[i] = randWDclassic(10);
@@ -26,16 +48,27 @@ genRandomTags = function(){
     return tags;
 };
 
+
+var typeList = genRandomTagsList(100, 200);
+var tagList = genRandomTagsList(500, 1000);
+
+var count = 0;
+
+
 createNode = function(_parent, _tags, _meta){
     var o = {parent: _parent, tags : _tags, meta : _meta};
+    count++;
+    if (count % 1000 == 0)
+        console.log(count);
+
     return Node.insert(o);
 };
 
 f = function(_parent, level, limit){
     if (level < limit){
         for (var i = 0; i < 10; i ++){
-            currentNode = createNode(_parent, genRandomTags(), genRandomMeta());
-            console.log(level);
+            var currentNode = createNode(_parent, genRandomTags(5, 10), genRandomMeta(5, 10));
+            //console.log(level);
             f(currentNode, level + 1, limit );
         }
     }
@@ -46,9 +79,8 @@ Node.remove({});
 
 
 if (Node.find().count() === 0) {
-
     for (var i = 0; i < 1; i++) {
-        f(null, 0, 4);
+        f(null, 0, 3);
     }
-
 }
+console.log(Node.find().count());
