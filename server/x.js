54312b67e17644fdd7afb3f49e8e39c01d451d52
sdanilov <sdanilov@kirkazan.ru>
2015-07-08 12:01:28 +0300

    [nf] Создание рандомного дерева

--- server/x.js
+++ server/x.js
@@ -5,42 +5,51 @@
     return s;
 };
 
-Node.remove({});
-
-if (Node.find().count() === 0) {
+genRandomMeta = function(){
     var meta = {};
     var count = Math.round((Math.random() * 10 + 10)); //количество параметров
 
+    for (var i = 0; i < count; i++){
+        meta[randWDclassic(10)] = randWDclassic(10);
+    }
+    return meta;
+};
 
-
-
-
-
+genRandomTags = function(){
+    var tags = [];
+    var count = Math.round((Math.random() * 10 + 10)); //количество тегов
 
     for (var i = 0; i < count; i++){
-        //Node.insert({title: randWDclassic(10)});
-        meta[randWDclassic(10)] = randWDclassic(10);
+        tags[i] = randWDclassic(10);
+    }
+    return tags;
+};
 
+createNode = function(_parent, _tags, _meta){
+    var id = new Meteor.Collection.ObjectID();
+    var o = {_id: id, parent: _parent, tags : _tags, meta : _meta};
+    Node.insert(o);
+    return id;
+};
+
+f = function(_parent, level, limit){
+    if (level < limit){
+        for (var i = 0; i < 10; i ++){
+            currentNode = createNode(_parent, genRandomTags(), genRandomMeta());
+            console.log(level);
+            f(currentNode, level + 1, limit );
+        }
+    }
+};
+
+
+Node.remove({});
+
+
+if (Node.find().count() === 0) {
+
+    for (var i = 0; i < 1; i++) {
+        f(null, 0, 3);
     }
 
-    Node.insert(meta);
-
-
-
-
-
-    //Node.insert({
-    //    title: 'Introducing Telescope',
-    //    url: 'http://sachagreif.com/introducing-telescope/'
-    //});
-    //
-    //Node.insert({
-    //    title: 'Meteor',
-    //    url: 'http://meteor.com'
-    //});
-    //
-    //Node.insert({
-    //    title: 'The Meteor Book',
-    //    url: 'http://themeteorbook.com'
-    //});
 }
