1cbfacb5d8cbc93c5fb9ab5e355ac5ada99e16bd
DanStopka <stepan@wintwin.ru>
2015-07-16 12:14:32 +0300

    [nf] packages

---
+++ server/old/b.js
@@ -0,0 +1,157 @@
+//function Root(data){
+//    var havingRoot = Tree.findOne({parent:null});
+//    if (havingRoot === undefined){
+//        var creationRootData = {parent:null};
+//        _.extend(creationRootData, data);
+//        Tree.insert(creationRootData);
+//        this.data = Tree.findOne({parent:null});
+//    } else {
+//        this.data = Tree.findOne({parent:null});
+//    }
+//
+//    this.createChild = function(data){
+//        return new Node(undefined, this.data._id, data);
+//    };
+//}
+//
+//
+///*
+// Node(id, parent, data)
+//  если заданы все параметры - возвращает ноду по ним (ничего не ищет и не создает)
+//  если задан только id - ищет по _id
+//  если id не задан - создает его и возвращает
+//
+// createChild(data)
+//  создает дочерний узел
+//
+// getParents()
+//  возвращает все родительские узлы массивом
+//
+//
+//
+//* */
+//
+//class Node {
+//    parent: string[];
+//    data: any;
+//    constructor (id, parent, data) {
+//
+//    }
+//
+//    createChild (data: any): Node {
+//
+//    }
+//}
+//
+//function Node(id, parent, data) {
+//    if (id === undefined) {
+//        var creationNodeData = {parent: _.flatten(parent)};
+//        _.extend(creationNodeData, data);
+//        var _id = Tree.insert(creationNodeData);
+//        this.data = Tree.findOne({_id: _id});
+//    } else {
+//
+//    }
+//
+//    if (id !== undefined && parent === undefined && data === undefined) {
+//        this.data = Tree.findOne({_id: id});
+//    }
+//
+//    if (id !== undefined && parent !== undefined && data !== undefined) {
+//        this.data = {_id: id, parent: parent, data: data};
+//    }
+//
+//
+//    this.createChild = function (data) {
+//        return new Node(undefined, this.data._id, data);
+//    };
+//
+//    this.getParents = function () {
+//        var r = [];
+//        if (this.data.parent instanceof Array) {
+//            var arr = Tree.find({
+//                _id: {$in: this.data.parent}
+//            }).fetch();
+//            for (var i = 0; i < arr.length; i++) {
+//                r.push(
+//                    new Node(arr[i]._id, arr[i].parent, _.omit(arr[i], '_id', 'parent'))
+//                );
+//            }
+//            return r;
+//        }
+//    };
+//
+//    this.addParents = function () {
+//        if (this.data.parent != null && arguments.length > 0) {
+//            var l = arguments.length;
+//            for (var i = 0; i < l; i++)
+//                this.data.parent.push(arguments[i].data._id);
+//        }
+//
+//    }
+//}
+//
+//_.extend(Node.prototype, {
+//   addParents: function () {
+//
+//   }
+//});
+//
+//
+//
+//
+//
+//Tree.remove({});
+//
+//var root = new Root({a:1, b:'2'});
+//
+////console.log('root - ',  root);
+//
+//var parent1 = root.createChild({node.js:'11111111'});
+////console.log('parent1 - ', parent1);
+//
+//var parent2 = root.createChild({node.js:'22222222'});
+////console.log('parent2 - ', parent2);
+//
+//
+//
+//var node = new Node(undefined,[parent1.data._id, parent2.data._id]);
+//
+//node.addParents(parent1, node);
+//node.addParents(node);
+//
+//
+////var arr = node.getParents();
+//
+//
+////parent1.addParents(parent1);
+//console.log(node);
+//
+//
+//
+////console.log(arr[0]);
+//
+//
+////console.log(node);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
---
+++ server/old/x.js
@@ -0,0 +1,86 @@
+﻿//
+//randWDclassic = function(n){
+//    var s ='', abd ='abcdefghijklmnopqrstuvwxyz0123456789', aL = abd.length;
+//    while(s.length < n)
+//        s += abd[Math.random() * aL|0];
+//    return s;
+//};
+//
+//
+//function randomNumber (from, to) {
+//    return Math.round((Math.random() * (to - from) + from));
+//}
+//var genRandomMeta = function(from, to){
+//    var meta = [];
+//    var count = randomNumber(from, to); //количество параметров
+//
+//    for (var i = 0; i < count; i++){
+//        var m = {
+//            _type: typeList[randomNumber(0, typeList.length)]
+//        };
+//        var fieldCount = randomNumber(10, 20);
+//        for (var j = 0; j < fieldCount; j++) {
+//            m[randWDclassic(10)] = randWDclassic(20);
+//        }
+//        meta[i] = m;
+//    }
+//    return meta;
+//};
+//
+//
+//var genRandomTags = function(from, to){
+//    var tags = [];
+//    var count = randomNumber(from, to); //количество тегов
+//
+//    for (var i = 0; i < count; i++){
+//        tags[i] = tagList[randomNumber(0, tagList.length)];
+//    }
+//    return tags;
+//};
+//
+//var genRandomTagsList = function(from, to){
+//    var tags = [];
+//    var count = randomNumber(from, to); //количество тегов
+//
+//    for (var i = 0; i < count; i++){
+//        tags[i] = randWDclassic(10);
+//    }
+//    return tags;
+//};
+//
+//
+//var typeList = genRandomTagsList(100, 200);
+//var tagList = genRandomTagsList(500, 1000);
+//
+//var count = 0;
+//
+//
+//createNode = function(_parent, _tags, _meta){
+//    var o = {parent: _parent, tags : _tags, meta : _meta};
+//    count++;
+//    if (count % 1000 == 0)
+//        console.log(count);
+//
+//    return Node.insert(o);
+//};
+//
+//f = function(_parent, level, limit){
+//    if (level < limit){
+//        for (var i = 0; i < 10; i ++){
+//            var currentNode = createNode(_parent, genRandomTags(5, 10), genRandomMeta(5, 10));
+//            //console.log(level);
+//            f(currentNode, level + 1, limit );
+//        }
+//    }
+//};
+//
+//
+//Node.remove({});
+//
+//
+//if (Node.find().count() === 0) {
+//    for (var i = 0; i < 1; i++) {
+//        f(null, 0, 3);
+//    }
+//}
+//console.log(Node.find().count());
